d739e9a1b120f53d8957d9d4dd812ee5315fd547
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-06-05 13:52:21 +0700

    Add developer's data

--- app/src/main/java/com/example/utspraktik_if11k_10118902_agungsepruloh/MainActivity.kt
+++ app/src/main/java/com/example/utspraktik_if11k_10118902_agungsepruloh/MainActivity.kt
@@ -5,6 +5,24 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.utspraktik_if11k_10118902_agungsepruloh.databinding.ActivityMainBinding
 
+/**
+ *
+ * =========================================================================================
+ * This project was created to fulfill the Midterm Exam of Mobile Computing Application course.
+ *
+ *                              Universitas Komputer Indonesia
+ * =========================================================================================
+ *
+ * Author's Information
+ * ==========================
+ * NIM      : 10118902
+ * Name     : Agung Sepruloh
+ * Class    : IF-11
+ * Informatics Engineering
+ * ==========================
+ * Created on  05 Jun 2021
+ *
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
