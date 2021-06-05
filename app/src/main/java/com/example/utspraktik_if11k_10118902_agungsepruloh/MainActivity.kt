bd54b69326b68f9534f1ff5ffb0767d040c3d330
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-06-05 10:10:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if11k_10118902_agungsepruloh/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if11k_10118902_agungsepruloh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
